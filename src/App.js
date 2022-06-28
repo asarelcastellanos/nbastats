c6ec07510e49a2ee29374e30982d278d3709079b
Asarel Castellanos <asarelc@gmail.com>
2022-06-28 15:43:44 -0700

    created site, showing nba data, finished searchbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+
+  let placeholder = ""; 
+
+  const [players, setPlayers] = useState([]);
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch(`https://www.balldontlie.io/api/v1/players?search=${searchTerm.replace(' ', '_')}`, {
+      method: "GET",
+      redirect: "follow",
+    })
+      .then((response) => response.json())
+      .then((result) => setPlayers(result.data))
+      .catch((error) => console.log("error", error));
+  }, [searchTerm]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <label>Find Player</label>
+        <input onChange={(e) => {
+          let value = e.target.value;
+          value.toLowerCase()
+          setSearchTerm(value);
+        }} value={searchTerm} type="text" name="searchTerm" placeholder="Ex: Lebron James"/>
+      </form>
+      {players?.map((player) => (
+        <div key={player.id}>
+          <h1>
+            {player.last_name}, {player.first_name}
+          </h1>
+          <p>{player.team.full_name}</p>
+          <p>{player.position === "" ? "No Player Position Avaliable": player.position}</p>
+          <p></p>
+        </div>
+      ))}
     </div>
   );
 }
